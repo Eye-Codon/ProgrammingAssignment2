7d3426d33396f95582f3fb1c401e7cdcd535ad19
Eye-Codon <30525002+Eye-Codon@users.noreply.github.com>
2019-03-11 01:06:38 +0530

    Update cachematrix.R
    this is a cached Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,37 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+  invMatrix <- NULL
+  setMatrix <- function(y)  #set value of the Matrix
+  {
+    x <<- y
+    invMatrix=NULL
+  }
+  getMatrix <- function() x                                #get value of the Matrix
+  setInverse <- function(inverse) invMatrix <<- inverse    #set value of the invertible matrix
+  getInverse <- function() invMatrix                        #get value of the invertible matrix
+  list(setMatrix = setMatrix, getMatrix = getMatrix,setInverse = setInverse,getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  ## Return a matrix that is the inverse of 'x'
+  
+  invMatrix <- x$getInverse()
+  if(!is.null(invMatrix))              #if inverse matrix is not NULL
+  {
+    message("getting cached data")
+    return(invMatrix)                  #return the cached invertible matrix
+  }
+  
+  #if condition is false  
+  Matrixdata <- x$getMatrix()
+  invMatrix <- solve(Matrixdata, ...)        # solve function to inverse the matrix
+  x$setInverse(invMatrix)                     # set the invertible matrix 
+  invMatrix                                    #return the invertible matrix
 }
